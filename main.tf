401e71461cdf11140182b71f663b038afd95b086
Sander van Harmelen <sander@vanharmelen.nl>
2019-08-02 21:27:26 +0200

    The quotes aren't needed, but lots of tools expect them

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource aws_kinesis_stream default {
+resource "aws_kinesis_stream" "default" {
   name                      = var.name
   shard_count               = var.shard_count
   retention_period          = var.retention_period
